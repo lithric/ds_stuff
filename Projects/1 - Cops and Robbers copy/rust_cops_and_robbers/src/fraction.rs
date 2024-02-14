2c61d09f2eaa9696c020643d7b1788c0fc06427f
lithric <35087325+lithric@users.noreply.github.com>
2024-02-14 00:57:43 -0500

    added more rs stuff

--- Projects/1 - Cops and Robbers copy/rust_cops_and_robbers/src/fraction.rs
+++ Projects/1 - Cops and Robbers copy/rust_cops_and_robbers/src/fraction.rs
@@ -3,7 +3,37 @@ struct Fraction {
     den: u16,
 }
 
-fn fraction(num: i32,den: u16) -> Fraction {
+const fn fraction(num: i32,den: u16) -> Fraction {
+    const fn abs(a:i32) -> u32 {
+        (if a >= 0 {a} else {-a}) as u32
+    }
+    const fn gcd(a:u32,b:u32) -> u32 {
+        if a%b==0 {b} else {gcd(b,a%b)}
+    }
+    Fraction {
+        num: num/(gcd(abs(num),den as u32) as i32),
+        den: ((den as u32)/gcd(abs(num),den as u32)) as u16,
+    }
+}
+/*
+static Fraction seqA(int series[],int mode,int k=0) {
+    Fraction returnFraction = 0;
+    if (mode==1) {
+        returnFraction = Fraction(1,series[k+1]);
+    }
+    else if (mode==2) {
+        returnFraction = seqA(series,1,k+1) + series[k+1];
+    }
+    else {
+        returnFraction = seqA(series,mode-1,k+1);
+        returnFraction += (mode%2==0?-1:1)*Fraction(1,seqB(series,mode-2,k)*seqARevProd(series,mode,k));
+    }
+    return returnFraction;
+}
+*/
+
+fn float_fraction(decimal: f32) -> Fraction {
+    if decimal<std::f32::EPSILON {return fraction(0,1)};
+    
     todo!()
 }
-
